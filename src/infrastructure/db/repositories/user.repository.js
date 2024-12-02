5f4f44f40be857fff1a579b14854a901eeb9c586
Antonio Lisbôa <antonion.2048@gmail.com>
2024-12-01 23:35:10 -0300

    fix: problem db ssl config

--- src/infrastructure/db/repositories/user.repository.js
+++ src/infrastructure/db/repositories/user.repository.js
@@ -11,6 +11,9 @@ class UserRepository {
       database: process.env.DB_DATABASE,
       password: process.env.DB_PASSWORD,
       port: process.env.DB_PORT,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     });
   }
 
